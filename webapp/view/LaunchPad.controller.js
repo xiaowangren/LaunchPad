409148e950c4a0c3e415e5f0135f39b74979e9bf
xiaowangren <wangren_0626@126.com>
2016-04-01 10:51:01 +0800

    first commit

---
+++ webapp/view/LaunchPad.controller.js
@@ -0,0 +1,24 @@
+sap.ui.controller("com.sap.ui5.view.LaunchPad", {
+	onInit:function(){
+		var usrid = jQuery.sap.getUriParameters().get("usrid").toUpperCase();
+		var sServiceUrl = "/sap/opu/odata/SAP/ZLAUNCHPAD_SRV/";
+        var oModel = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
+        sap.ui.getCore().setModel(oModel);
+		var mParameters = {};
+		var comId = this.getView().byId("container");
+		mParameters['async'] = true;
+		mParameters['success'] = jQuery.proxy(function(sRes) {
+			var json = sRes.results;
+			console.log(json);
+			var oModelJson = new sap.ui.model.json.JSONModel();
+			oModelJson.setData({"json":json});
+			comId.setModel(oModelJson);
+		});
+		mParameters['error'] = jQuery.proxy(function(eRes) {
+			sap.m.MessageToast.show("网络未连接");
+		}, this);
+		
+		//http://znbb-erpd-01.zhenergy.com.cn:8000/sap/opu/odata/SAP/ZLAUNCHPAD_SRV/EE_PAD_SET?$filter=Uname eq 'ac-wangjx'
+		sap.ui.getCore().getModel().read("/EE_PAD_SET?$filter=(Uname eq '"+usrid+"')", mParameters);
+	}            
+});
